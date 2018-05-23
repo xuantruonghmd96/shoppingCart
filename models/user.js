1386b62b7651605943a9e1e4734d48025d10e1ff
xuantruonghmd96 <xuantruonghmd96@gmail.com>
2018-05-23 20:19:09 +0700

    add method encrypt password to the user schema model, use package bcrypt-nodejs

--- models/user.js
+++ models/user.js
@@ -1,9 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new Schema({
     email: {type: string, required: true},
     password: {type: string, required: true}
 });
+
+userSchema.method.encryptPassword = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+}
+
+userSchema.method.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+}
 
 module.exports = mongoose.model('user', userSchema);
