0fde35488ca1ff7bda9a4ceb49fd93ebfc6997e8
xuantruonghmd96 <xuantruonghmd96@gmail.com>
2018-05-23 16:18:16 +0700

    load product from database

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+mongoose.connect('mongodb://truong:truong@ds229609.mlab.com:29609/shoppingcart')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+  product.find(function(err, data) {
+    res.render('shop/index', { title: 'Express', products: data });
+  });
 });
 
 module.exports = router;
