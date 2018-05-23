82a8df9791a5c90505964f43e037c91f236976d0
xuantruonghmd96 <xuantruonghmd96@gmail.com>
2018-05-23 16:27:44 +0700

    use express-session

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var product = require('../models/product');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   product.find(function(err, data) {
-    res.render('shop/index', { title: 'Express', products: data });
+    res.render('shop/index', { title: 'Shopping Cart', products: data });
   });
 });
 
